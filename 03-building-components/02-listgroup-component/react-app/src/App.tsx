4a20af548435510d7346163e4e91d66c8ea80df9
benj-lazaro <benjie.work@gmail.com>
2024-08-07 17:47:10 +0800

    Section 02 listgroup component initial commit

---
+++ 03-building-components/02-listgroup-component/react-app/src/App.tsx
@@ -0,0 +1,9 @@
+import Message from "./Message";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  return <div><ListGroup /></div>
+}
+
+// Export the React App component 
+export default App
