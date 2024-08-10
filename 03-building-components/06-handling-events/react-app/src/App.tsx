9802f08bbd76b3a510c78333e38038cd45f08f7e
benj-lazaro <benjie.work@gmail.com>
2024-08-10 23:56:04 +0800

    Section 06 handling events initial commit

---
+++ 03-building-components/06-handling-events/react-app/src/App.tsx
@@ -0,0 +1,12 @@
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  );
+}
+
+// Export the React App component
+export default App;
