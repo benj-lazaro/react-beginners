5607f4eae4fcab4f8ddd2caf9e9459fa9cf1b654
benj-lazaro <benjie.work@gmail.com>
2024-08-13 21:12:01 +0800

    Section 11 passing children initial commit

---
+++ 03-building-components/11-passing-children/react-app/src/App.tsx
@@ -0,0 +1,15 @@
+// Passing children to a component
+
+import Alert from "./components/Alert";
+
+function App() {
+  return (
+    <div>
+      <Alert>
+        <h1>Hello World</h1>
+      </Alert>
+    </div>
+  );
+}
+
+export default App;
