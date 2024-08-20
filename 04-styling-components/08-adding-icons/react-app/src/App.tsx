dd86092c376926735d9637184a2af08b95e94d88
benj-lazaro <benjie.work@gmail.com>
2024-08-20 15:18:04 +0800

    Section 08 adding icons initial commit

---
+++ 04-styling-components/08-adding-icons/react-app/src/App.tsx
@@ -0,0 +1,12 @@
+import { BsFillCalendarFill } from "react-icons/bs";
+
+function App() {
+  return (
+    <div>
+      {/* Set prop color to Red & size to 40 */}
+      <BsFillCalendarFill color="red" size="40" />
+    </div>
+  );
+}
+
+export default App;
