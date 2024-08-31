55e088eecaee5154d928c20a01b17358091d475b
benj-lazaro <benjie.work@gmail.com>
2024-08-31 23:49:45 +0800

    Section 08 content refactor

--- 06-building-forms/08-schema-validation/react-app/src/components/Form.tsx
+++ 06-building-forms/08-schema-validation/react-app/src/components/Form.tsx
@@ -1,10 +1,14 @@
 import React, { FormEvent, useState } from "react";
 import { useForm, FieldValues } from "react-hook-form";
+import { z } from "zod";
 
-interface FormData {
-  name: string;
-  age: number;
-}
+// Define the schema by passing a configuration object
+const schema = z.object({
+  name: z.string().min(3),
+  age: z.number().min(18),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
   // Get a form object
