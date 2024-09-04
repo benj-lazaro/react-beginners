5c9030dd263dbfd2b541bcc8a1fe51563136364f
benj-lazaro <benjie.work@gmail.com>
2024-09-04 23:42:20 +0800

    Section 06 handline events content refactor

--- 03-building-components/06-handling-events/react-app/src/components/ListGroup.tsx
+++ 03-building-components/06-handling-events/react-app/src/components/ListGroup.tsx
@@ -7,11 +7,15 @@ function ListGroup() {
   const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
+    // Fragment implementation
     <>
       <h1>List</h1>
-      {items.length === 0 && <p>No item found</p>}
+
+      {/* Conditional rendering using a Logical AND operator */}
+      {items.length === 0 && <p>No items found</p>}
 
       <ul className="list-group">
+        {/* Renders <li> element for each item in the array */}
         {items.map((item, index) => (
           <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
@@ -22,4 +26,5 @@ function ListGroup() {
   );
 }
 
+// Export the function as a default object from this module (i.e. component)
 export default ListGroup;
