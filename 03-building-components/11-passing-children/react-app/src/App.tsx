a7f2fb4ccaf2aeb18c0d7237306a7b77d41932e9
benj-lazaro <benjie.work@gmail.com>
2024-09-07 23:41:09 +0800

    Section 11 passing children content refactor

--- 03-building-components/11-passing-children/react-app/src/App.tsx
+++ 03-building-components/11-passing-children/react-app/src/App.tsx
@@ -1,15 +1,14 @@
-// Passing children to a component
-
 import Alert from "./components/Alert";
 
 function App() {
   return (
-    <div>
+    <div className="alert alert-primary">
       <Alert>
-        <h1>Hello World</h1>
+        <h1>Hello World!</h1>
       </Alert>
     </div>
   );
 }
 
+// Export the React App component
 export default App;
