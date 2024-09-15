cb84a8dc5a8ec859370edac15cc896e6aa8fcc42
benj-lazaro <benjie.work@gmail.com>
2024-09-15 18:03:51 +0800

    Section 05 understanding strict mode content refactor

--- 05-manage-component-state/05-understand-strict-mode/react-app/src/App.tsx
+++ 05-manage-component-state/05-understand-strict-mode/react-app/src/App.tsx
@@ -1,12 +1,15 @@
-import Message from "./components/Message";
+import Message from "./Message";
 
 function App() {
-  
   return (
     <div>
+      {/* Strict mode renders the each call to the <Message /> component twice */}
       <Message />
+      {/* <Message />
+      <Message /> */}
     </div>
   );
 }
 
+// Export the React App component
 export default App;
