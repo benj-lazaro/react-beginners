eebcff41e830fa629426f1478321f4f2ca30f796
benj-lazaro <benjie.work@gmail.com>
2024-09-16 15:22:31 +0800

    Section 06 updating objects content refacor

--- 05-manage-component-state/06-updating-objects/react-app/src/App.tsx
+++ 05-manage-component-state/06-updating-objects/react-app/src/App.tsx
@@ -1,23 +1,26 @@
 import { useState } from "react";
 
-
 function App() {
-  // State Hook
+  // State hook
   const [drink, setDrink] = useState({
     title: "Americano",
-    price: 5
+    price: 5,
   });
 
+  // Event handler
   const handleClick = () => {
-    setDrink({...drink, price: 6});
+    setDrink({ ...drink, price: 6 }); // New state object with updated value of targeted property
   };
-  
+
   return (
     <div>
-      <button onClick={ handleClick }>Click Me</button>
-      {drink.price}
+      <p>
+        {drink.title} Price: {drink.price}
+      </p>
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
 
+// Export the React App component
 export default App;
