6242622b1d980b37fa77f6e2546cdb311e4a639a
benj-lazaro <benjie.work@gmail.com>
2024-09-16 18:37:17 +0800

    Section 07 updating nested objects content refactor

--- 05-manage-component-state/07-updating-nested-objects/react-app/src/App.tsx
+++ 05-manage-component-state/07-updating-nested-objects/react-app/src/App.tsx
@@ -1,30 +1,35 @@
 import { useState } from "react";
 
-
 function App() {
-  // State Hook of a nested object
+  // State hook
   const [customer, setCustomer] = useState({
-    name: "John",
+    name: "John Wick",
     address: {
       city: "San Francisco",
-      zipCode: 94111
-    }
+      zipCode: 12345,
+    },
   });
 
-  // Event handler to update the property zipCode
+  // Event handler
   const handleClick = () => {
-    setCustomer({...customer, address: {...customer.address, zipCode: 94222}})
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipCode: 94222 },
+    });
   };
-  
+
   return (
     <div>
-      {customer.name} <br />
-      {customer.address.city} <br />
-      {customer.address.zipCode} <br />
-      <button onClick={ handleClick }>Click Me</button>
-      
+      <p>Name: {customer.name}</p>
+
+      <p>
+        Address: {customer.address.city}, {customer.address.zipCode}
+      </p>
+
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
 
+// Export the React App component
 export default App;
