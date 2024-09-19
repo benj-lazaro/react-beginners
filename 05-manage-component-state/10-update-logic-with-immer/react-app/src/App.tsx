8a197f4dd1cc145020cd2a37093665b0f47afd3b
benj-lazaro <benjie.work@gmail.com>
2024-09-19 17:43:35 +0800

    Section 10 update logic with immer content refactor

--- 05-manage-component-state/10-update-logic-with-immer/react-app/src/App.tsx
+++ 05-manage-component-state/10-update-logic-with-immer/react-app/src/App.tsx
@@ -1,28 +1,35 @@
 import { useState } from "react";
-import { produce } from "immer";
+import produce from "immer";
 
 function App() {
-  // State Hook for the array of objects
+  // State hook
   const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
+    { id: 1, title: "Bug1", fixed: false },
+    { id: 2, title: "Bug2", fixed: false },
   ]);
 
   // Event handler
-  // Use Immer to handle the update state of the targeted object element in the array
   const handleClick = () => {
-    setBugs(produce(draft => {
-      const bug = draft.find(bug => bug.id === 1)
-      if (bug) bug.fixed = true;
-    }));
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+
+        if (bug) bug.fixed = true;
+      })
+    );
   };
 
   return (
     <div>
-      { bugs.map(bug => <p key={bug.id}> {bug.title} {bug.fixed ? "Fixed" : "New"} </p>) }
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "New"}
+        </p>
+      ))}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
 
+// Export the React App component
 export default App;
