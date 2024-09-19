1e58950c6b7c7e6f2dfd08335684d0f64d7c7d46
benj-lazaro <benjie.work@gmail.com>
2024-09-19 21:13:50 +0800

    Section 11 sharing data content refactor

--- 05-manage-component-state/11-sharing-state/react-app/src/App.tsx
+++ 05-manage-component-state/11-sharing-state/react-app/src/App.tsx
@@ -2,19 +2,20 @@ import { useState } from "react";
 import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 
-
 function App() {
-  // State Hook
+  // State hook
   const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
- 
+
   // Event handler
+  const handleClick = () => {};
 
   return (
     <div>
       <NavBar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])}/>
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])}></Cart>
     </div>
   );
 }
 
+// Export the React App component
 export default App;
