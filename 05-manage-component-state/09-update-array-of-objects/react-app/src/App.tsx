01b82e84d1319231c2b804e94f951d5359f6bfa8
benj-lazaro <benjie.work@gmail.com>
2024-09-19 14:44:22 +0800

    Section 09 update array of objects content refactor

--- 05-manage-component-state/09-update-array-of-objects/react-app/src/App.tsx
+++ 05-manage-component-state/09-update-array-of-objects/react-app/src/App.tsx
@@ -1,22 +1,24 @@
 import { useState } from "react";
 
 function App() {
-  // State Hook of an array
+  // State hook
   const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
+    { id: 1, title: "Bug1", fixed: false },
+    { id: 2, title: "Bug2", fixed: false },
   ]);
 
-  // Event handler to udpate the fixed property of the object with an id of 1
+  // Event handler
   const handleClick = () => {
     setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
   };
 
   return (
     <div>
+      <p>View State Hook's state update in React Development Tools</p>
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
 
+// Export the React App component
 export default App;
