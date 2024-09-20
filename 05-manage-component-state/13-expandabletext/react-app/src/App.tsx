d5391164704892e1928e32bb37b12a4d61453449
benj-lazaro <benjie.work@gmail.com>
2024-09-20 16:55:25 +0800

    Section 13 expandabletext content refactor

--- 05-manage-component-state/13-expandabletext/react-app/src/App.tsx
+++ 05-manage-component-state/13-expandabletext/react-app/src/App.tsx
@@ -2,17 +2,25 @@ import ExpandableText from "./components/ExpandableText";
 
 function App() {
   // State Hook
-  
+
   // Event handler
-  const clickHandler = () => {
-  };
+  const clickHandler = () => {};
 
   return (
     <div>
       <ExpandableText>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident, id, et impedit repellat consequuntur corporis unde exercitationem aliquid nam pariatur laudantium ratione tempore doloribus vel odio doloremque commodi! Libero illo incidunt quae sint asperiores dolorum ut inventore voluptate commodi quam tenetur debitis totam vitae sed, excepturi amet dignissimos facilis corrupti ratione ab. Provident sapiente fuga maiores, recusandae tempore iusto enim. Recusandae numquam rerum vitae, quisquam illo molestias harum asperiores odio reprehenderit veniam qui, odit cumque nulla. Ducimus exercitationem ipsa illum ratione quo aspernatur harum ex perspiciatis odit, delectus at aperiam! Magni adipisci quas illum suscipit nam sint. Voluptatum, labore voluptas.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident, id,
+        et impedit repellat consequuntur corporis unde exercitationem aliquid
+        nam pariatur laudantium ratione tempore doloribus vel odio doloremque
+        commodi! Libero illo incidunt quae sint asperiores dolorum ut inventore
+        voluptate commodi quam tenetur debitis totam vitae sed, excepturi amet
+        dignissimos facilis corrupti ratione ab. Provident sapiente fuga
+        maiores, recusandae tempore iusto enim. Recusandae numquam rerum vitae,
+        quisquam illo molestias harum asperiores odio reprehenderit veniam qui,
+        odit cumque nulla. Ducimus exercitationem ipsa illum ratione quo
+        aspernatur harum ex perspiciatis odit, delectus at aperiam! Magni
+        adipisci quas illum suscipit nam sint. Voluptatum, labore voluptas.
       </ExpandableText>
-
     </div>
   );
 }
--- 05-manage-component-state/13-expandabletext/react-app/src/components/ExpandableText.tsx
+++ 05-manage-component-state/13-expandabletext/react-app/src/components/ExpandableText.tsx
@@ -1,18 +1,27 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 interface Props {
-    children: string;
-    maxChars?: number;
+  children: string;
+  maxChars?: number;
 }
 
-const ExpandableText = ({children, maxChars = 100}: Props) => {
-    const [isExpanded, setExpanded] = useState(false);
+const ExpandableText = ({ children, maxChars = 100 }: Props) => {
+  const [isExpanded, setExpanded] = useState(false);
 
-    if (children.length <= maxChars) return <p>{children}</p>;
+  // Initial summarization of the text content (i.e. children)
+  if (children.length <= maxChars) return <p>{children}</p>;
 
-    const text = isExpanded ? children : children.substring(0, maxChars) 
+  // Toggle switch for contracting / expanding the text content (i.e. children)
+  const text = isExpanded ? children : children.substring(0, maxChars);
 
-    return <p>{text}...<button onClick={() => setExpanded(!isExpanded)}>{isExpanded ? "Less" : "More"}</button></p>
-}
+  return (
+    <p>
+      {text}...
+      <button onClick={() => setExpanded(!isExpanded)}>
+        {isExpanded ? "Less" : "More"}
+      </button>
+    </p>
+  );
+};
 
-export default ExpandableText
+export default ExpandableText;
