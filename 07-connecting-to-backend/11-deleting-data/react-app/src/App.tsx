75904182c9b53b4f9f38986d6aab63df5bf9c4e4
benj-lazaro <benjie.work@gmail.com>
2024-10-12 23:40:52 +0800

    Section 11 deleting data initial commit

---
+++ 07-connecting-to-backend/11-deleting-data/react-app/src/App.tsx
@@ -0,0 +1,79 @@
+import axios, { Axios, CanceledError } from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const App = () => {
+  // State hook
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  // Effect hook
+  useEffect(() => {
+    // Clean-up function
+    const controller = new AbortController();
+
+    // Show the loading indicator
+    setLoading(true);
+
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+
+      {isLoading && <div className="spinner-border"></div>}
+
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default App;
