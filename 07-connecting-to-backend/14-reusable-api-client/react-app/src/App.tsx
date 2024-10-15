77f5a2e43ca1b9c905f5e461759be395a89f319a
benj-lazaro <benjie.work@gmail.com>
2024-10-15 16:48:27 +0800

    Section 14 reusable api client initial commit

---
+++ 07-connecting-to-backend/14-reusable-api-client/react-app/src/App.tsx
@@ -0,0 +1,118 @@
+import { useEffect, useState } from "react";
+import apiClient, { CanceledError } from "./services/api-client";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const App = () => {
+  // State hook
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  // Effect hook
+  useEffect(() => {
+    // Clean-up function
+    const controller = new AbortController();
+
+    // Show the loading indicator
+    setLoading(true);
+
+    apiClient
+      .get<User[]>("/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    apiClient.delete("/users/" + user.id).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
+
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 0, name: "Mosh" };
+
+    setUsers([newUser, ...users]);
+
+    apiClient
+      .post("/users", newUser)
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + "!" };
+
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+
+    apiClient.patch("/users/" + user.id, updatedUser).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
+
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+
+      {isLoading && <div className="spinner-border"></div>}
+
+      <button className="btn btn-primary mb-3" onClick={addUser}>
+        Add
+      </button>
+
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+
+            <div>
+              <button
+                className="btn btn-outline-secondary mx-1"
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default App;
