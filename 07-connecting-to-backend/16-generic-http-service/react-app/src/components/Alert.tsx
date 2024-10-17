e6b24289bc1258192d157b4ec6f15c35ef51f502
benj-lazaro <benjie.work@gmail.com>
2024-10-17 23:34:37 +0800

    Section 16 generic http service initial commit

---
+++ 07-connecting-to-backend/16-generic-http-service/react-app/src/components/Alert.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+
+// Refactor Props, add a prop onClose that monitors for an event & informs React when it occurred
+interface Props {
+  children: ReactNode;
+  onClose: () => void;
+}
+
+const Alert = ({ children, onClose }: Props) => {
+  return (
+    <div className="alert alert-primary alert-dismissible">
+      {children}
+
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      ></button>
+    </div>
+  );
+};
+
+export default Alert;
